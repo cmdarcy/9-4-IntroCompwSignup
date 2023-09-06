2ff3877402d02d0fa620bf14252d554ae7e74256
Christopher d'Arcy <cdarcy656481@gmail.com>
2023-09-06 10:48:09 -0400

    fix error icon positioning, second pass on js form validation

--- index.js
+++ index.js
@@ -4,47 +4,109 @@ const firstNameVal = document.querySelector('[data-namefield]')
 const lastNameVal = document.querySelector('[data-lastNamefield]')
 const emailVal = document.querySelector('[data-emailfield]')
 const pwVal = document.querySelector('[data-pwfield]')
-const nameRegex = /[0-9]/g;
 
-const nameCheck = (input) => {
+
+const nameCheck = input => {
+    const nameRegex = /[0-9]/g;
+    const errorIcon = input.parentElement.querySelector('svg')
+    const errorLabel = input.parentElement.querySelector('div')
     console.log(`Starting value: "${input.value}"`)
     if (input.value == '') {
-        console.log(`${input.value} is blank!`)
-        input.style.border = '1px solid var(--Red)'
+        input.classList.remove('success-border')
+        errorIcon.classList.remove('success')
+        errorLabel.classList.remove('success')
+        input.classList.add('error-border')
+        errorIcon.classList.add('error')
+        errorLabel.classList.add('error')
+        errorLabel.innerText = 'Name cannot be empty!'
         return;
     }
     if (input.value.search(nameRegex) === -1) {
-        console.log(`${input.value} doesn't have a number, is an ok name`)
+        input.classList.remove('error-border')
+        errorIcon.classList.remove('error')
+        errorLabel.classList.remove('error')
+        input.classList.add('success-border')
+        errorIcon.classList.add('success')
+        errorLabel.classList.add('success')
     } else {
-        console.log(`${input.value} contains a number`)
-        input.style.border = '1px solid var(--Red)'
+        input.classList.remove('success-border')
+        errorIcon.classList.remove('success')
+        input.classList.add('error-border')
+        errorIcon.classList.add('error')
+        errorLabel.classList.remove('success')
+        errorLabel.classList.add('error')
+        errorLabel.innerText = 'Name cannot contain a number!'
     }
 }
 
 const emailCheck = input => {
-    console.log(`Starting value: "${input.value}"`)
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const errorIcon = input.parentElement.querySelector('svg')
+    const errorLabel = input.parentElement.querySelector('div')
     if (input.value == '') {
-        console.log(`${input.value} is blank!`)
-        input.style.border = '1px solid var(--Red)'
+        input.classList.remove('success-border')
+        errorIcon.classList.remove('success')
+        input.classList.add('error-border')
+        errorIcon.classList.add('error')
+        errorLabel.classList.remove('success')
+        errorLabel.classList.add('error')
+        errorLabel.innerText = 'Email cannot be empty!'
         return;
     }
-    if (input.value === 'Test') {
-        console.log(`Good email address!`)
+    if (emailRegex.test(String(input.value).toLowerCase())) {
+        input.classList.remove('error-border')
+        errorIcon.classList.remove('error')
+        errorLabel.classList.remove('error')
+        input.classList.add('success-border')
+        errorIcon.classList.add('success')
+        errorLabel.classList.add('success')
     } else {
-        console.log(`Bad email adress`)
-        input.style.border = '1px solid var(--Red)'
-        input.style.color = 'var(--Red)'
+        input.classList.remove('success-border')
+        errorIcon.classList.remove('success')
+        input.classList.add('error-border')
+        errorIcon.classList.add('error')
+        errorLabel.classList.remove('success')
+        errorLabel.classList.add('error')
         input.value = 'email@example.com'
+        errorLabel.innerText = 'Looks like this is not an email!'
+    }
+}
+
+const pwCheck = input => {
+    const errorIcon = input.parentElement.querySelector('svg')
+    const errorLabel = input.parentElement.querySelector('div')
+    if (input.value == '') {
+        input.classList.remove('success-border')
+        errorIcon.classList.remove('success')
+        input.classList.add('error-border')
+        errorIcon.classList.add('error')
+        errorLabel.classList.remove('success')
+        errorLabel.classList.add('error')
+        errorLabel.innerText = 'Password cannot be empty!'
+        return;
+    }
+    if (input.value.length < 8) {
+        input.classList.remove('success-border')
+        errorIcon.classList.remove('success')
+        input.classList.add('error-border')
+        errorIcon.classList.add('error')
+        errorLabel.classList.remove('success')
+        errorLabel.classList.add('error')
+        errorLabel.innerText = 'Password must be at least 8 characters!'
+    } else {
+        input.classList.remove('error-border')
+        errorIcon.classList.remove('error')
+        errorLabel.classList.remove('error')
+        input.classList.add('success-border')
+        errorIcon.classList.add('success')
+        errorLabel.classList.add('success')
     }
 }
 
 submitButton.addEventListener('click', (ev) => {
-    errorIcons[0].style.opacity = 1
-    errorIcons[1].style.opacity = 1
-    errorIcons[2].style.opacity = 1
-    errorIcons[3].style.opacity = 1
+    ev.preventDefault();
     nameCheck(firstNameVal);
     nameCheck(lastNameVal);
     emailCheck(emailVal);
-    pwVal.style.border = '1px solid var(--Red)'
+    pwCheck(pwVal);
 })
